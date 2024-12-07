8601fbcf4b47f6016ce31a7a623405ba1bb9fd47
Cole Patrick <coleyo34@gmail.com>
2024-12-07 12:52:01 -0600

    rearranged some stuff, removed useless import of <string>

--- speed.cpp
+++ speed.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <cmath>
 #include "speed.hpp"
 using namespace std;
--- speed.hpp
+++ speed.hpp
@@ -8,13 +8,15 @@ private:
   double actualSpeedArr[100]; // Holds the estimated value of speed based on the two datasets.
   double avgAcc;
 
+  
+public:
+  speed(); // Constructs the speed object, doing the four below methods.
+  ~speed(); // No pointers or heap objects, so this simply reports the deconstruct.
+
   void openFiles(); // Retrieves both data files and stores them in gpsSpeed and watchSpeed
   void calcAcc(); // Calculates the avgAcc based on the average difference between each data point.
   void findErrors(); // Reads each data point in each speed array, and reports them as an anomaly if they are outside of a 3 standard deviation range.
   void actualSpeed(); // Based on avgAcc, generates an estimated speed for each time.
-public:
-  speed(); // Constructs the speed object, doing all the above private functions.
-  ~speed(); // No pointers or heap objects, so this simply reports the deconstruct.
 
   void searchByTime(int); // Given a time, prints all data at that time.
   void searchByGPSSpeed(int); // Given a speed, finds the closest value in the dataset.
